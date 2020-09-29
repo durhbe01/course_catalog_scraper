22dfc725f28b81b0c584bbd4f7526a17b300c612
Ben Durham <bendurham441@gmail.com>
2020-09-29 11:57:57 -0400

    Started filtering to write into file, still need to add more filtering with page structure anomalies (allow both divs and mains to be the type for the content)

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 This script will eventually grab all of the relevant content for the course catalog from the gettysburg.edu
 website and compile it into a single HTML file
+
+Removed 2 pages
--- main.py
+++ main.py
@@ -8,16 +8,29 @@ with open('pages.csv') as pageList:
   f = open("output.html", "a")
   count = 0
   for row in listReader:
+    above_featured = False
     if row[0] == "URL":
       continue
-    if count < 1:
+    if row[2] == 'Until Featured course header':
+      above_featured = True
+    if count >= 0:
       page = urllib.request.urlopen(row[0])
       print(row[0])
       count += 1
       soup = BeautifulSoup(page.read(), 'html.parser')
-      content = soup.find('div', {'id': 'content'})
-      columns = content.findChildren('div', {'class': 'column'})
-      f.write(str(columns))
-      print(columns[1])
-    f.close()
+      content = soup.find('div', {'id':'content'})
+      try:
+        columns = content.find_all('div', {'class': 'column'})
+        test = columns[1]
+      except IndexError:
+        print("index")
+      if above_featured:
+        above_featured = columns[1].find('div', {'class': 'gb-u-spacing-bottom'})
+        f.write(str(above_featured))
+      else:
+        try:
+          f.write(str(columns[1]))
+        except IndexError:
+          print(str(columns))
+  f.close()
 
